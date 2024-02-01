15fb0fe93f5f3fedada999ba1d5ac613993c8438
denispena7 <penadenis007@gmail.com>
2024-02-01 14:04:29 +0100

    Primer commit

---
+++ src/es/studium/ejercicio8/CajaBlanca.java
@@ -0,0 +1,19 @@
+package es.studium.ejercicio8;
+
+public class CajaBlanca 
+{
+	public static int posicion(String letra, String cadena)
+	{
+		int posicion = 1;
+		int resultado = 0;
+		for (int i = 0; i < cadena.length(); i++)
+		{
+			if (cadena.substring(i, i + 1).equals(letra))
+			{
+				resultado = posicion;
+			}
+			posicion++;
+		}
+		return resultado;
+	}
+}
---
+++ src/es/studium/ejercicio8/TestCajaBlanca.java
@@ -0,0 +1,14 @@
+package es.studium.ejercicio8;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestCajaBlanca {
+
+	@Test
+	void camino1() {
+		
+	}
+
+}
